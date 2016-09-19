69c3254c84ccb1c061d16378462e4308bad6446a
Thorsten <thorsten@potpiejimmy.de>
2016-09-19 16:59:03 +0200

    implemented header ui

--- client/app.component.ts
+++ client/app.component.ts
@@ -7,14 +7,21 @@ import { MenuItem }  from 'primeng/primeng';
   templateUrl: 'pt/client/app.html'
 })
 export class AppComponent {
-  title = 'Angular 2 With Express and PrimeNG';
 
     private menuitems: MenuItem[];
 
     ngOnInit() {
         this.menuitems = [
-            {label: 'Main', icon: 'fa-bar-chart', routerLink: ['/main']},
-            {label: 'Export', icon: 'fa-calendar', routerLink: ['/export']}
+            {label: 'Main', routerLink: ['/main']},
+            {label: 'Export', routerLink: ['/export']},
+            {label: 'Domains', routerLink: ['/domains']},
+            {label: 'Projects', routerLink: ['/projects']},
+            {label: 'Budgets', routerLink: ['/budgets']},
+            {label: 'Booking Templates', routerLink: ['/templates']},
+            {label: 'FI', routerLink: ['/adminfi']},
+            {label: 'Budget Control', routerLink: ['/budgetcontrol']},
+            {label: 'Budget Plans', routerLink: ['/budgetplans']},
+            {label: 'Forecasts', routerLink: ['/forecasts']}
         ];
     }
 }
