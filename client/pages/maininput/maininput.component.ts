994735a1676cfb41a07068f5391b94403db7c003
Thorsten <thorsten@potpiejimmy.de>
2016-10-18 17:32:51 +0200

    implemented getter and setter for formatted date

--- client/pages/maininput/maininput.component.ts
+++ client/pages/maininput/maininput.component.ts
@@ -11,7 +11,7 @@ export class MainInputComponent {
     data = null;
     users = [];
 
-    current = {"day": "01 Sep 2016"};
+    current = {"day": Date.now()};
 
     constructor(private loginService : LoginService) { }
 
@@ -34,5 +34,20 @@ export class MainInputComponent {
 
     dateChanged(newValue) {
         console.info("Date changed to: "+newValue);
+    }
+
+    formatDate():string {
+        let d = new Date(this.current.day);
+        let months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+        return ("0"+d.getDate()).substr(-2) + " " + months[d.getMonth()] + " " + d.getFullYear(); 
+    }
+
+    get currentDay():string {
+        return this.formatDate() + "                        :" + this.current.day;
+    }
+
+    set currentDay(d:string) {
+        this.current.day = parseInt(d.substr(d.indexOf(":")+1));
+        console.info("set date " + this.current.day);
     }
 }
